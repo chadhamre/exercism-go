e88e6b7c5dddbd08e21e0882ff87c6ae94d5fb1a
Chad Hamre <chadhamre@gmail.com>
2019-05-27 16:40:37 -0700

    feat: complete leap and hamming

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,4 +1,22 @@
 package hamming
 
+import "errors"
+
+// Distance will return the hamming difference b/w two strings
 func Distance(a, b string) (int, error) {
+
+	// check if strings match
+	if len(a) != len(b) {
+		return -1, errors.New("string length does not match")
+	}
+
+	// check if each char matches
+	var distance int
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			distance++
+		}
+	}
+	return distance, nil
+
 }
